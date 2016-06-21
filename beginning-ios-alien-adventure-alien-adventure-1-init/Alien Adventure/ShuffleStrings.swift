974d08b84336e3a5e54a3e9290290b67e13454e7
alirezasamar <alireza.smr@gmail.com>
2016-06-21 15:13:10 +0800

    shuffleStrings Implemented

--- beginning-ios-alien-adventure-alien-adventure-1-init/Alien Adventure/ShuffleStrings.swift
+++ beginning-ios-alien-adventure-alien-adventure-1-init/Alien Adventure/ShuffleStrings.swift
@@ -9,6 +9,33 @@
 extension Hero {
     
     func shuffleStrings(s1 s1: String, s2: String, shuffle: String) -> Bool {
+
+        //Compare string lengths
+        let initialString = s1 + s2
+        if initialString.characters.count != shuffle.characters.count {
+            return false
+        }
+        
+        var generatedString1 = ""
+        var generatedString2 = ""
+        
+        //Generate two strings from shuffle
+        for c in shuffle.characters {
+            if s1.containsString(String(c)) {
+                generatedString1.append(c)
+            } else {
+                generatedString2.append(c)
+            }
+        }
+        
+        //Compare the generatedString with original s1 & s2
+        if s1 != generatedString1 {
+            return false
+        }
+        if s2 != generatedString2 {
+            return false
+        }
+        
         return true
     }
 }
